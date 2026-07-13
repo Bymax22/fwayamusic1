82d5622edb4c5fbf07113c294e6966595daba845
Bymax <kwibisa21@gmail.com>
2026-07-13 03:11:38 +0200

    Fix media module

--- apps/backend/src/playlist/playlist.module.ts
+++ apps/backend/src/playlist/playlist.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { PlaylistController } from './playlist.controller';
 import { PlaylistService } from './playlist.service';
 import { PrismaModule } from '../db/prisma.module';
+import { EventsModule } from '../events/events.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, EventsModule],
   controllers: [PlaylistController],
   providers: [PlaylistService],
 })
