eb9b487e3c5f1b33f9efb86a16af17ad1155bb39
Bymax <kwibisa21@gmail.com>
2026-07-13 03:05:38 +0200

    Fix media 2

--- apps/backend/src/media-interaction/media-interaction.module.ts
+++ apps/backend/src/media-interaction/media-interaction.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MediaInteractionController } from './media-interaction.controller';
 import { MediaInteractionService } from './media-interaction.service';
 import { PrismaModule } from '../db/prisma.module';
+import { EventsModule } from '../events/events.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, EventsModule],
   controllers: [MediaInteractionController],
   providers: [MediaInteractionService],
 })
